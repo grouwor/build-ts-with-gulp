daca873fc4d231fdf886f1a0fecef7ce5ea374d3
Robin Weber <grouwor@gmail.com>
2023-06-05 17:53:56 -0800

    Separate sayHello() into a module

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-function hello(compiler) {
-    console.log("Hello from ".concat(compiler));
-}
-hello('TypeScript');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greet_1 = require("./greet");
+console.log((0, greet_1.sayHello)('TypeScript'));
