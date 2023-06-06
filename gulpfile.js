c1b265bdac75b81d86393e85d1f2f9740794c804
Robin Weber <grouwor@gmail.com>
2023-06-06 12:01:38 -0800

    Build with Terser

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var fancyLog = require('fancy-log');
+var terser = require('gulp-terser');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var tsify = require('tsify');
 var paths = {
     pages: ['src/*.html']
@@ -12,20 +14,20 @@ function copyHTML() {
     return gulp.src(paths.pages).pipe(gulp.dest('dist'));
 }
 
-var watchifiedBrowserifyTS = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-}).plugin(tsify));
-
-function bundle() {
-    return watchifiedBrowserifyTS
+exports.default = gulp.parallel(copyHTML, function() {
+    return browserify({
+        basedir: ".",
+        debug: true,
+        entries: ["src/main.ts"],
+        cache: {},
+        packageCache: {},
+      })
+        .plugin(tsify)
         .bundle()
-        .on("error", fancyLog)
         .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(terser())
+        .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest("dist"));
-}
-
-watchifiedBrowserifyTS.on('update', bundle);
-watchifiedBrowserifyTS.on('log', fancyLog);
-exports.default = gulp.parallel(copyHTML, bundle);
+});
