67648f73a5357525483aa99e25f30e1fd9a3b419
Robin Weber <grouwor@gmail.com>
2023-06-06 01:32:58 -0800

    Browserify TS With HTML, combine multiple tasks

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greet_1 = require("./greet");
-console.log((0, greet_1.sayHello)('TypeScript'));
+function displayHello(divId, name) {
+    var elt = document.getElementById(divId);
+    if (elt) {
+        elt.innerText = (0, greet_1.sayHello)(name);
+    }
+    else {
+        throw new Error("Cannot find an element with the specified id ".concat(divId));
+    }
+}
+displayHello('greet', 'TypeScript');
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
-
-exports.default = function() {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var paths = {
+    pages: ['src/*.html']
 };
+
+function copyHTML() {
+    return gulp.src(paths.pages).pipe(gulp.dest('dist'));
+}
+
+function browserifyWithTS() {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+}
+
+exports.default = gulp.parallel(copyHTML, browserifyWithTS);
