c9f392f603628af766824beec9d51765dba3dd2f
Robin Weber <grouwor@gmail.com>
2023-06-05 18:01:15 -0800

    Change main.ts to update the page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { sayHello } from './greet';
 
-console.log(sayHello('TypeScript'));
+function displayHello(divId: string, name: string) {
+    const elt = document.getElementById(divId);
+    if (elt) {
+        elt.innerText = sayHello(name);
+    } else {
+        throw new Error(`Cannot find an element with the specified id ${divId}`);
+    }
+}
+displayHello('greet', 'TypeScript');
